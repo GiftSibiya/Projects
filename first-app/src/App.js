5d16e376b98b8ffcf296fbc698db10fc14fd53d0
GiftSibiya <sibiya.bobo@gmail.com>
2023-12-17 06:07:46 +0200

    added some buttons

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -2,7 +2,15 @@ import React from "react";
 import TodoList from "./TodoList";
 
 function App() {
-  return <TodoList />;
+  return (
+    <>
+      <TodoList />;
+      <input type="text" />
+      <button>Add Item</button>
+      <button>Clear Item</button>
+      <div>0 left to do</div>
+    </>
+  );
 }
 
 export default App;
